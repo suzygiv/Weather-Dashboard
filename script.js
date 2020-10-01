39f9445b9c54322aff474ab3c8640eef1f1f2b79
suzannegivnish@gmail.com <suzannegivnish@gmail.com>
2020-10-01 12:52:32 -0400

    updating script.js file

--- script.js
+++ script.js
@@ -46,68 +46,23 @@ let APIKey = "7b3dea9f51f9521ad1e328c3862e4254";
 
 $(function() {
 
-const inputEl = document.getElementById("city-input");
-const searchEl = document.getElementById("searchCity");
-const nameEl = document.getElementById("city-name");
-const currentTempEl = document.getElementById("temperature");
-const currentHumidityEl = document.getElementById("humidity");4
-const currentWindEl = document.getElementById("windSpeed");
-const currentUVEl = document.getElementById("uvIndex");
-const historyEl = document.getElementById("history");
-let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("searchCity");
+    const nameEl = document.getElementById("city-name");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("windSpeed");
+    const currentUVEl = document.getElementById("uvIndex");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+
+    const APIKey = "7b3dea9f51f9521ad1e328c3862e4254";
 
     console.log(searchHistory);
     var currentDate = (moment().format('LLL'));
     $("#current-weather").append(currentDate);
 
-        //When I click the search button, I am presented with current and future conditions. 
-        $("button").on("click", function() {
-            var searchCity = $("#searchCity").val();
-            $("#searchCity").val("");
-            searchWeather(searchCity);
-        });
-    
-        function searchWeather(searchCity) {
-    
-        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 
-        console.log(queryURL);
-        console.log(searchCity);
     
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-    
-                $("#currentCity").text(searchCity);
-    
-                var temperature = $("<p>").text("Temperature: " + response.main.temp + "°F");
-    
-                $('#currentCity').append(temperature);
-    
-                var humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
-    
-                temperature.append(humidity);
-    
-                var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + "MPH");
-    
-                humidity.append(temperature);
-    
-            });
-            
-        
-        }
-    
-        var history = JSON.parse(window.localStorage.getItem("history")) || [];
-        if (history.length > 0) {
-            searchWeather(history[history.length - 1]);
-        }
-    
-        for ( var i = 0; i > history.length; i++){
-            historyRow(history[i]); 
-        }
-            
-    });
-
 
